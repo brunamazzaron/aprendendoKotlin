cbb24d3a1d3c7aa8dd835576c3efc86fc7face4f
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2022-05-15 21:16:16 -0300

    Estudando sobre Kotlin

---
+++ src/main/kotlin/Mainkt.kt
@@ -0,0 +1,43 @@
+var mensagem = "Bem vindo ao bytebank"
+var titular = "Bruna"
+var conta = 12345
+var saldo = -4.0
+
+fun bytebank(){
+    println("$mensagem, Olá $titular, número da conta: $conta, saldo da conta: R$$saldo,00")
+}
+
+fun testaCondicoes(saldo: Double){
+    if( saldo > 0.0){
+        println(" USANDO IF ->> Conta positiva")
+    }else if(saldo == 0.0){
+        println("USANDO IF ->> Conta neutra")
+    }else{
+        println("USANDO IF ->> Conta negativa")
+    }
+
+    //Lembre-se que o plugin do Kotlin para o IntelliJ verifica as possíveis conversões. Para isso, utilize o atalho Alt + Enter quando aparecer o código sublinhado.
+    when {
+        saldo > 0.0 -> {
+            println("USANDO WHEN ->> Conta positiva")
+        }
+        saldo == 0.0 -> {
+            println("USANDO WHEN ->> Conta neutra")
+        }
+        else -> {
+            println("USANDO WHEN ->> Conta negativa")
+        }
+    }
+
+    //Lembra um switch case
+    when {
+        saldo > 0.0 -> println("USANDO WHEN EXPRESSION->> Conta positiva")
+        saldo == 0.0 -> println("USANDO WHEN EXPRESSION ->> Conta neutra")
+        else -> println("USANDO WHEN EXPRESSION ->> Conta negativa")
+    }
+}
+
+fun main() {
+    bytebank()
+    testaCondicoes(saldo)
+}
