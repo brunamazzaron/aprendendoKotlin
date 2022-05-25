99269b8ff1be69bf0ca5ff01aba604bcc29ee67b
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2022-05-24 22:10:53 -0300

    entendendo constructor

--- src/main/kotlin/Mainkt.kt
+++ src/main/kotlin/Mainkt.kt
@@ -2,18 +2,22 @@
 //As classes são as responsáveis por modificar seus atributos.
 //A boa prática de orientação a objetos é que qualquer comportamento
 // que ajuste o estado da classe (modifique os atributos), deve ser implementado pela própria classe.
-class Conta{
-    var titular = ""
-    var numeroConta = 0
+class Conta(titular: String, numeroConta: Int){
+    var titular = titular //Inicializacao de propriets
+    var numeroConta = numeroConta
     var saldo = 0.0
-        private set
+        private set // só nossa classe pode modificar
     //se quiser criar uma representaçao de uma pessoa em crio uma pessoa, cria as regras
     //sempre acessa por meio de um set ou um get
     //field receve o valor inicial
     //chamado de proprites
     // serve para alterar o valor
 
-    constructor()
+    //RESPONSAVEL POR CRIAR/vida A NOSSA CLASSE
+//    constructor(titular: String, numeroConta: Int){
+//        this.titular
+//        this.numeroConta
+//    }
 
     //funçao membro de uma classe, métodos ou comportamentos da classe
     fun deposita(valor: Double){
@@ -51,27 +55,19 @@ fun admConta(){
     //Ao trabalhar com Orientação a Objetos, precisamos ficar atentos ao usar uma variável por conta da atribuição por cópia ou referência.
     // Ao alterar uma variável de um objeto, todas as variáveis que apontam para o objeto enxergam o mesmo valor.
     //Utilizar uma referência, qualquer modificação que é feita no objeto é único para todas as variáveis que apontam para a referência.
-    val contaJoao = Conta() // eu instanciei a classe Conta()
-    contaJoao.titular = "-----João-----"
-    contaJoao.numeroConta = 123
+    val contaJoao = Conta(titular = "-----João-----",numeroConta = 123) // eu instanciei a classe Conta()
     contaJoao.deposita(600.00)
     println("Conta Joao:${ contaJoao.titular }, ${contaJoao.numeroConta}, ${contaJoao.saldo}")
 
-    val contaMaria = Conta()
-    contaMaria.titular = "-----Maria-----"
-    contaMaria.numeroConta = 122
+    val contaMaria = Conta(titular = "-----Maria-----",numeroConta = 122)
     contaMaria.deposita(900.00)
     println("Conta Maria:${contaMaria.titular}, ${ contaMaria.numeroConta }, ${contaMaria.saldo}")
 
-    val contaAlex = Conta()
-    contaAlex.titular = "----Alex-----"
-    contaAlex.numeroConta = 111
+    val contaAlex = Conta(titular = "----Alex-----", numeroConta = 111)
     contaAlex.deposita(4000.00)
     println("Conta Alex:${ contaAlex.titular }, ${contaAlex.numeroConta}, ${contaAlex.saldo}")
 
-    val contaFran = Conta()
-    contaFran.titular = "----Fran----"
-    contaFran.numeroConta = 133
+    val contaFran = Conta( titular = "----Fran----", numeroConta = 133)
     contaFran.deposita(5000.00)
     println("Conta Fran:${contaFran.titular}, ${ contaFran.numeroConta }, ${contaFran.saldo}")
 
@@ -95,6 +91,7 @@ fun admConta(){
     contaMaria.saca(20.00)
 
     contaAlex.saca(1000.00)
+    //contaAlex é um objeto chamando um comportamento saca
     contaFran.saca(1000.00)
 
     println("Saca: ----Alex-----${ contaAlex.titular }, ${contaAlex.numeroConta}, ${contaAlex.saldo}")
@@ -102,14 +99,12 @@ fun admConta(){
 
     println("-----Transferencia-----")
     if(contaAlex.transferir(200.00, contaFran)){
-        println("Sucesso")
         println("transferencia: ----Alex----${ contaAlex.titular }, ${contaAlex.numeroConta}, ${contaAlex.saldo}")
         println("transferencia: ----Fran-----${contaFran.titular}, ${ contaFran.numeroConta }, ${contaFran.saldo}")
     }else if(contaMaria.transferir(200.00, contaJoao)) {
         println("Conta Joao:${contaJoao.titular}, ${ contaJoao.numeroConta }, Transferência: ${contaJoao.saldo}")
         println("Conta Maria:${contaMaria.titular}, ${ contaMaria.numeroConta }, Transferência: ${contaMaria.saldo}")
     }else if(contaJoao.transferir(900.00, contaMaria)){
-        println("sucesso!")
         println("Conta Maria:${contaMaria.titular}, ${ contaMaria.numeroConta }, Transferência: ${contaMaria.saldo}")
         println("Conta Joao:${contaJoao.titular}, ${ contaJoao.numeroConta }, Transferência: ${contaJoao.saldo}")
     }else{
@@ -135,32 +130,35 @@ fun admConta(){
 
 //
 
-data class Pessoa( val name: String,  val id: Number,  val cpf: String,  val prof: String){
-    constructor(name: String) : this(name,1,"", "")
-    constructor(name: String, prof: String) : this(name,2,"", prof)
-    constructor(enfermeiro: Enfermeiro) : this(enfermeiro.name, enfermeiro.prof)
-}
-
-data class Enfermeiro(val name: String, var prof: String) {
-
-    fun medicar(){
-        println("medicado")
-    }
-}
+//data class Pessoa( val name: String,  val id: Number,  val cpf: String,  val prof: String){
+//    constructor(name: String) : this(name,1,"", "")
+//    constructor(name: String, prof: String) : this(name,2,"", prof)
+//    constructor(enfermeiro: Enfermeiro) : this(enfermeiro.name, enfermeiro.prof)
+//}
+//
+//data class Enfermeiro(val name: String, var prof: String) {
+//
+//    fun medicar(){
+//        println("medicado")
+//    }
+//}
 
 
 fun main() {
-    //admConta()
-    val pessoa1 = Pessoa( "Bruna", 123, "1239484", "Professor")
-    println(pessoa1)
-
-    val pessoa2 = Pessoa(name = "Clara")
-    println(pessoa2)
-
-    val enfermeiro1 = Enfermeiro("Cleber", "Enfermeiro" )
-    enfermeiro1.medicar()
-
-    val pessoa3 = Pessoa(enfermeiro1)
-    println(pessoa3)
+    admConta()
+//    val pessoa1 = Pessoa( "Bruna", 123, "1239484", "Professor")
+//    println(pessoa1)
+//
+//    val pessoa2 = Pessoa(name = "Clara")
+//    println(pessoa2)
+//
+//    val enfermeiro1 = Enfermeiro("Cleber", "Enfermeiro" )
+//    enfermeiro1.medicar()
+//
+//    val pessoa3 = Pessoa(enfermeiro1)
+//    println(pessoa3)
 
 }
+
+//encapsulamento: ultilizando o getter e setter para acessar campo saldo é uma tecnica conhecida como
+// Ao encapsular um atributo, apenas a classe é capaz de modificar o valor, como preferir.
